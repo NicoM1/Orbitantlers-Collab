bc56d86a3ae449075830324a6dd8facf702792ec
NicoM1 <nico.may99@gmail.com>
2014-12-24 22:58:22 -0800

    lock camera

--- src/player/Player.hx
+++ src/player/Player.hx
@@ -7,9 +7,14 @@ import phoenix.Texture;
 
 import luxe.components.sprite.SpriteAnimation;
 
+import luxe.Input;
+
 class Player extends Sprite {
 
 	var _camSpeedMult: Float = 2.0;
+
+	var _lockCamera: Bool = false;
+	var _camSpeed: Float = 100;
 
 	public function new() {
 		var texture = Luxe.loadTexture('assets/art/character/run_strip.png');
@@ -27,10 +32,23 @@ class Player extends Sprite {
 	}
 
 	override function update(dt: Float) {
-		var dist: Vector = Vector.Subtract(pos, Luxe.camera.pos);
 
-		Luxe.camera.pos.x += (dist.x - Luxe.screen.w / 2) * dt * _camSpeedMult;
-		Luxe.camera.pos.y += (dist.y - Luxe.screen.h / 2) * dt * _camSpeedMult;
+		if(Luxe.input.keypressed(Key.key_c)) {
+			_lockCamera = !_lockCamera;
+		}
+
+		if(!_lockCamera) {
+			var dist: Vector = Vector.Subtract(pos, Luxe.camera.pos);
+
+			Luxe.camera.pos.x += (dist.x - Luxe.screen.w / 2) * dt * _camSpeedMult;
+			Luxe.camera.pos.y += (dist.y - Luxe.screen.h / 2) * dt * _camSpeedMult;
+		}
+		else {
+			if(Luxe.mouse.x < 10) Luxe.camera.pos.x -= dt * _camSpeed;
+			if(Luxe.mouse.x > Luxe.screen.w - 10) Luxe.camera.pos.x += dt * _camSpeed;
+			if(Luxe.mouse.y < 10) Luxe.camera.pos.y -= dt * _camSpeed;
+			if(Luxe.mouse.y > Luxe.screen.h - 10) Luxe.camera.pos.y += dt * _camSpeed;
+		}
 	}
 
 	function _createAnim() {
